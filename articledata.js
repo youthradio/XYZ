44169b54d21769bbfb71525d29b4526455f602fc
clayxavier <32530001+clayxavier@users.noreply.github.com>
2017-11-07 17:58:31 -0800

    Added Content

--- articledata.js
+++ articledata.js
@@ -13,14 +13,14 @@ var topics = {
 	P2:"Technology",
 	P3:"Fashion",
 	P4:"Slang",
-	P5:"Music / Culture",
+	P5:"Music",
 }
 var topicDescriptions = {
-	P1:"The American political system is a drama that can keep anyone guessing -- and no, we're not just talking about House Of Cards. Take a trip down the red, white and blue memory lane, and hear what a difference a decade can make.",
-	P2:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-	P3:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-	P4:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	P5:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	P1:"Take a trip down the red, white and blue memory lane. What a difference a decade can make in politics!",
+	P2:"MySpace vs. Snapchat, livejournal vs. group text. Listen to find your spot along teen tech culture divides.",
+	P3:"Looking back on the last few generations of teen fashion trends... some things stood the test of time better than others.",
+	P4:"Teens of every generation know: it's not just what you say, it's how you say it.",
+	P5:"From radio to MTV to YouTube, every generation finds a way to get its groove on.",
 }
 
 //Colors
@@ -60,27 +60,27 @@ var genZColors = {
 //Article titles
 
 var genXTitles = {
-	P1:"Youth Reflect on Obama Victory",
-	P2:"Gen X Are Wild!",
-	P3:"Gen X Are Uhhhhhhhh!",
-	P4:"Gen X Are okay",
-	P5:"Gen X!",
+	P1:"Punks for Bush",
+	P2:"A Video Game Addict Reviews The Sega Dreamcast",
+	P3:"Lose The Levis",
+	P4:"Slang Predictions For The Future",
+	P5:"Wanna be an MTV VJ?",
 }
 
 var genYTitles = {
-	P1:"Can Paul Ryan get the youth vote?",
-	P2:"Gen Y Are Wild!",
-	P3:"Gen Y Are Uhhhhhhhh!",
-	P4:"Gen Y Are okay",
-	P5:"Gen Y!",
+	P1:"Hope Is The New Rebellion",
+	P2:"Public is the new private",
+	P3:"One Teen's Fashion Plea: Stop Sagging",
+	P4:"The N-word: Slang Or Disrespect?",
+	P5:"Song of the Summer: Crazy",
 }
 
 var genZTitles = {
 	P1:"Donald Trump Takes Office",
-	P2:"Gen Z Are Wild!",
-	P3:"Gen Z Are Uhhhhhhhh!",
-	P4:"Gen Z Are okay",
-	P5:"Gen Z!",
+	P2:"Snapchat Sexting?",
+	P3:"Teens say looks can be liberating despite fashion police",
+	P4:"What's The Meaning Of The Gun Emoji?",
+	P5:"Punks Are The New Mall Rats",
 }
 
 //Audio files
@@ -138,28 +138,28 @@ var genZIMG = {
 //Dates
 
 var genXDate = {
-	P1:"2008",
-	P2:"null",
-	P3:"null",
-	P4:"null",
-	P5:"null",
+	P1:"2004",
+	P2:"1999",
+	P3:"1999",
+	P4:"2004",
+	P5:"1999",
 }
 
 var genYDate = {
-	P1:"2012",
-	P2:"null",
-	P3:"null",
-	P4:"null",
-	P5:"null",
+	P1:"2008",
+	P2:"2008",
+	P3:"2005",
+	P4:"2007",
+	P5:"2006",
 }
 
 
 var genZDate = {
 	P1:"2017",
-	P2:"null",
-	P3:"null",
-	P4:"null",
-	P5:"null",
+	P2:"2013",
+	P3:"2014",
+	P4:"2015",
+	P5:"2014",
 }
 
 
@@ -167,27 +167,27 @@ var genZDate = {
 
 
 var genXDesc = {
-	P1:"Yes we did. Barack Obama became the first black President of the United States on a campaign of hope.",
-	P2:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-	P3:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-	P4:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	P5:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	P1:"Who says conservatives can't be inclusive? At the 2004 Republican convention in NYC, Youth Radio found some punks with an appreciation for George W. himself.",
+	P2:"Its a step up from the N64, but will the Sega Dreamcast video game console last the test of time? This gamer review takes you back to simpler times.",
+	P3: "It's all about that button fly.” And according to one '90s teen, this classic jean brand was simply past its prime.",
+	P4:"High Schooler Sanovia Jackson rang in 2004 by talking with NPR host Melissa Block what slang phrases were in and out the new year.",
+	P5:"MTV was at the height of fashion and music… for the '90s. Youth Radio listened in on an MTV VJ audition in 1999.",
 }
 
 var genYDesc = {
-	P1:"From when Paul Ryan was the fresh-faced, guitar-playing, political candidate of 2012 conservatives' dreams.",
-	P2:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-	P3:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-	P4:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	P5:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	P1:"Back in  2008, Senator Barack Obama was a Presidential hopeful running a fresh campaign based on hope. Hear young people at the Democratic National Convention react to that promise.",
+	P2:"The teen trend in 2008? Moving from email to Facebook wall posts. From the early days of social media receipts.",
+	P3:"Before skinny jeans were a thing, one Oakland teen was on a mission to get his friends to give up the sag.",
+	P4:"In 2007, NPR asked a bunch of people to weigh in on the n-word -- including this group of Bay Area teens.",
+	P5:"Before Gnarls Barkley's song became the tune you couldn't get out of your head, it was an underrated hit of the summer of 2006.",
 }
 
 var genZDesc = {
 	P1:"In a stunning political upset, Donald J.Trump became President #45 -- with many liberals still asking, 'What just happened?'",
-	P2:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-	P3:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-	P4:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	P5:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	P2:"An introduction to the then-brand new app Snapchat -- listen to teens reassure adults it's not ALL about the d**k pics.",
+	P3:"Social media isn't the only place teens feel the pressure to look good. The high school hallway is also place where your look can lift you up, or bring you down.",
+	P4:"They say a picture can be worth a thousand words. But what happens when that picture is a tiny gun you sometimes use to text your friends?",
+	P5:"Meet me at the mall isn't really a thing anymore… unless you're talking about the punk music scene in Stockton, California.",
 }
 
 
